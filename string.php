8506a72e15c62e70be3ab6bc65e21c32da4d8e99
shakilkhan2045 <shakilkhanfv@gmail.com>
2024-08-23 20:43:00 +0600

    frist commit of the Assignment-2 question-1

---
+++ string.php
@@ -0,0 +1,45 @@
+<?php   
+   
+
+   function vowelCount($item){  //declaring function to count vowels
+
+           $vowels = ['a', 'e', 'i', 'o', 'u'];      //declaring the vowels.
+           $count = 0 ;                             //count variable declared store the number of vowel if there is. 
+           
+           foreach (str_split($item) as $letter ) {
+              foreach($vowels as  $vowel){
+                if($vowel == $letter){
+                    $count++;
+                }
+              }
+          }
+          echo "Vowel Count: ".$count.", " ;
+   };
+
+   
+   //declaring the reverse string function
+   function reverseStr($item){ //Note aurgument must be a string here
+       echo  "Reversed String: ".strrev($item);
+   };
+
+
+
+
+
+
+
+   $strings = ["Hello", "World", "PHP", "Programming"];
+
+   for ($item =0 ;  $item < sizeof( $strings ) ; $item++) { 
+
+    echo "Original String: " .$strings[$item]." ," ;
+   
+   
+   
+    vowelCount($strings[$item]);//calling the vowelCount function.
+    reverseStr($strings[$item]);//calling the reverseStr function
+    
+    
+    
+    echo PHP_EOL;
+  }

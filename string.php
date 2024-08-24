7be1e94eeeedff8c56f898fdca90e826cc2c1d98
shakilkhan2045 <shakilkhanfv@gmail.com>
2024-08-24 23:27:43 +0600

    optimize structure  and organized the functions in order and added   strtoLowercase to item   so that vowel array item reduced just a e i o u . line break added to output look like exactly

--- string.php
+++ string.php
@@ -1,48 +1,71 @@
 <?php   
    
 
-   function vowelCount($item){  //declaring function to count vowels
 
-           $vowels = ['a','e', 'i', 'o','u','A','E','I','O','U']; //declaring the vowels (UPPERCASE AND LOWERCASE).
-            
-           //note:alternate:  by converting the input string to lowercase by string method  uppercase letters can be avoided.
+   //function to print original strings
+   function originalString($originalString){
+ 
+    echo "Original String: " .$originalString." ," ;
+  
+     }
+    
 
-           $count = 0 ;                             //count variable declared store the number of vowel if there is. 
+
+     
+   //declaring function to count vowels
+   function vowelCount($item){ 
+
            
-           foreach (str_split($item) as $letter ) {
-              foreach($vowels as  $vowel){
-                if($vowel == $letter){
-                    $count++;
-                }
-              }
+    //  $vowels = ['a','e', 'i', 'o','u','A','E','I','O','U']; //declaring the vowels (UPPERCASE AND LOWERCASE).
+                         
+    //note:alternate:  by converting the input string to lowercase by string method  uppercase letters can be avoided.
+
+
+    $vowels = ['a','e','i','o','u'];
+
+     $count = 0 ;                             //count variable declared store the number of vowel if there is. 
+     
+     foreach (str_split($item) as $letter ) {
+        foreach($vowels as  $vowel){
+
+          
+          if (strtolower($letter) == $vowel){
+              $count++;
           }
-          echo "Vowel Count: ".$count.", " ;
-   };
+        }
+    }
+    echo "Vowel Count: ".$count.", " ;
+}
 
+
+
+//declaring the reverse string function
+function reverseStr($item){ //Note aurgument must be a string here
+  echo  "Reversed String: ".strrev($item);
+}
    
-   //declaring the reverse string function
-   function reverseStr($item){ //Note aurgument must be a string here
-       echo  "Reversed String: ".strrev($item);
-   };
 
 
 
+   function countVowelAndPrintRev($strings){
 
+    for ($i =0 ;  $i< sizeof( $strings ) ; $i++) { 
 
+    echo PHP_EOL; 
 
-
-   $strings = ["Hello", "World", "PHP", "Programming"];
-
-   for ($item =0 ;  $item < sizeof( $strings ) ; $item++) { 
-
-    echo "Original String: " .$strings[$item]." ," ;
-   
-   
-   
-    vowelCount($strings[$item]);//calling the vowelCount function.
-    reverseStr($strings[$item]);//calling the reverseStr function
-    
-    
+    $item = $strings[$i];
+    originalString($item);
+    vowelCount($item);//calling the vowelCount function.
+    reverseStr($item);//calling the reverseStr function
     
     echo PHP_EOL;
   };
+
+
+}
+
+//input strings which will be checked to count the vowel 
+$strings = ["Hello", "World", "PHP", "Programming"]; // declared  The  array of strings/words
+
+
+countVowelAndPrintRev($strings);
